12560560c00ed8cb3caab5949a5ee0e23297fab5
BellsForHer <arhall88@gmail.com>
2023-03-05 18:05:30 -0600

    bug fix

--- app/models/template.rb
+++ app/models/template.rb
@@ -21,10 +21,6 @@ class Template < ApplicationRecord
         end
     end
 
-    class User < ApplicationRecord
-        has_one_attached :file do |attachable|
-          attachable.variant :thumb, resize_to_limit: [100, 100]
-        end
-      end
+ 
       
 end
